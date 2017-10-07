8e80bf511e87429b973c265aec74c25f2ab0a1d1
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-10-07 07:12:30 -0500

    archivos base

---
+++ PATS-32-6-sumaNumeros/src/pats/pkg32/pkg6/sumanumeros/PATS326SumaNumeros.java
@@ -0,0 +1,20 @@
+/*
+ * Ejercicio 6: Pedir números hasta que se teclee un 0, mostrar la suma de todos
+ * los números introducidos.
+ */
+package pats.pkg32.pkg6.sumanumeros;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS326SumaNumeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
